360e14d2f6dabe476ea777e53e338c1d68b10808
MaxiCorrea <jmaxicorrea@gmail.com>
2024-02-06 22:30:42 -0300

    Installed @maxicorrea UI Kit NPM packages

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { UI } from "./views/UI";
+import * as serviceWorker from "./serviceWorker";
+
+ReactDOM.render(
+   <UI />, 
+  document.getElementById("root")
+);
+  
+serviceWorker.register();
